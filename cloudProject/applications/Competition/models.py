c14b6575e0ccff63ea8ed4f71bf71c5576c0bbb6
camiloco <jc.zuluaga140@uniandes.edu.co>
2015-09-09 23:20:13 -0500

    models has been created

--- cloudProject/applications/Competition/models.py
+++ cloudProject/applications/Competition/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+#from .. import Company
 
-# Create your models here.
+from django.utils.encoding import smart_unicode
+
+class Competition(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.CharField(max_length=255)
+    startDate =models.DateTimeField(auto_now=False, auto_now_add=True)
+    endDate =models.DateTimeField(auto_now=False, auto_now_add=True)
+    description = models.CharField(max_length=255)
+    url = models.CharField(max_length=255)
+    #company = models.ForeignKey(Company)
+
+    def __unicode__(self):
+        title = "%s" % (smart_unicode(self.name))
+        return title
