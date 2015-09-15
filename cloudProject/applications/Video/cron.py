78aff3f2c0acc2ca55b405303c9f4f32b3bb57b3
Jhonatan Alarcon <jhonatan.alarcon.ariza@gmail.com>
2015-09-15 13:57:24 -0500

    Se realiza ajuste de cron

--- cloudProject/applications/Video/cron.py
+++ cloudProject/applications/Video/cron.py
@@ -1,12 +1,22 @@
 import kronos
-import random
+
+from cloudProject.applications.Video.models import Video
+
 
 __author__ = 'jhonatan'
 
-@kronos.register('*/1 * * * *')
-def complain():
-    complaints = [
-        "I forgot to migrate our applications's cron jobs to our new server! Darn!",
-        "I'm out of complaints! Damnit!"
-    ]
-    print random.choice(complaints)
+
+@kronos.register('* * * * *')
+def convert_videos():
+    list_video = Video.objects.filter(state='WFC')
+    print len(list_video)
+    if len(list_video) > 0:
+        try:
+            for video_wfc in list_video:
+                print video_wfc.title + ' .'
+
+        except ValueError:
+            print 'There is a error for this process'
+
+    else:
+        print 'There are Not videos for converted'
