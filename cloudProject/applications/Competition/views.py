1a4e0981ad18c68f0b3c9b641d21517905a78e25
camilo <camiloco@gmail.com>
2015-10-24 16:11:30 -0500

    create competitio

--- cloudProject/applications/Competition/views.py
+++ cloudProject/applications/Competition/views.py
@@ -3,94 +3,78 @@ from datetime import datetime
 from django.shortcuts import render_to_response, RequestContext, HttpResponseRedirect, render
 from django.db.models import Q
 from django.utils.timezone import utc
-from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.views.generic import TemplateView
+from cloudProject.applications.Account.session import Session
 
-
-#Impor auth
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
 
 #Import Models
-from cloudProject.applications.Competition.models import Competition
+from cloudProject.applications.MongoDB_APP.Company import Company
+from cloudProject.applications.MongoDB_APP.Competition import Competition
 
-#Import Forms
+# Import Forms
 from cloudProject.applications.Competition.forms import CreateNewCompetition
 
 
-@login_required(login_url='/account/forbbiden/')
 def index(request):
+    if Session().verify_current_session(request.COOKIES['userId']):
+        print "entro a index"
+        company ="ca1"
 
-    try:
-        user_id = request.user.id
-        company = User.objects.get(id=user_id)
-    except:
-        company = User.objects.get(id=-1)
+        form = CreateNewCompetition()
 
-    form = CreateNewCompetition()
+        competitions_list = Company().get_competitions(company)
 
-    competitions_list = Competition.objects.filter(Q(company=company) & Q(active=True))
+        paginator = Paginator(competitions_list, 50)
 
-    paginator = Paginator(competitions_list, 50)
+        page = request.GET.get('page')
 
-    page = request.GET.get('page')
+        try:
+            competitions = paginator.page(page)
+        except PageNotAnInteger:
+            # If page is not an integer, deliver first page.
+            competitions = paginator.page(1)
+        except EmptyPage:
+            # If page is out of range (e.g. 9999), deliver last page of results.
+            competitions = paginator.page(paginator.num_pages)
 
-    try:
-        competitions = paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an integer, deliver first page.
-        competitions = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range (e.g. 9999), deliver last page of results.
-        competitions = paginator.page(paginator.num_pages)
+        ctx= {'competitions':competitions,'form': form}
 
-    ctx= {'competitions':competitions,'form': form}
-
-    if request.method == 'POST':
-        print request.POST['name']
-        print request.FILES['image']
-        print request.POST['description']
-        print request.POST.get('startDate')
-        print request.POST.get('endDate')
-        form = CreateNewCompetition(request.POST, request.FILES)
-        #if form.is_valid():
-        print (request.POST.get('startDate'))
-        print('valid')
-        new_competition = Competition(name=request.POST['name'],
+        if request.method == 'POST':
+            form = CreateNewCompetition(request.POST, request.FILES)
+            #if form.is_valid():
+            Competition().create(name=request.POST['name'],
                                       image=request.FILES['image'],
                                       description=request.POST['description'],
                                       startDate=request.POST.get('startDate'),
                                       endDate=request.POST.get('endDate'),
                                       url='url',
-                                      active=True,
-                                      company=company)
-        new_competition.save(form)
-        new_competition.url="/competitions/" + str(new_competition.id)
-        new_competition.save()
+                                      active=True)
 
-        ctx['resp'] = 'OK'
+            ctx['resp'] = 'OK'
 
-        return HttpResponseRedirect("/competitions")
-        #else:
-         #   ctx['resp'] = 'BAD'
-          #  print form.is_valid
-    return render_to_response('Competition/index.html', ctx, context_instance=RequestContext(request))
+            return HttpResponseRedirect("/competitions")
+            #else:
+             #   ctx['resp'] = 'BAD'
+              #  print form.is_valid
+        return render_to_response('Competition/index.html', ctx, context_instance=RequestContext(request))
+    else:
+        print "entro a forbidden"
+        return render_to_response("Error/forbidden.html")
+
 
 class finish(TemplateView):
-
-    def post(self,request,id_competition):
-
+    def post(self, request, id_competition):
         c = Competition.objects.get(id=id_competition)
-        c.endDate=datetime.utcnow().replace(tzinfo=utc)
+        c.endDate = datetime.utcnow().replace(tzinfo=utc)
         c.active = False
         c.save()
 
         return HttpResponseRedirect("/competitions")
 
+
 class edit(TemplateView):
-
-    def post(self,request,id_competition):
-
+    def post(self, request, id_competition):
         c = Competition.objects.get(id=id_competition)
         c.name = request.POST['nameedit']
         c.description = request.POST['descriptionedit']
@@ -100,8 +84,7 @@ class edit(TemplateView):
 
         return HttpResponseRedirect("/competitions")
 
+
 def show_all_competitions(request):
     list_competitions = Competition.objects.filter(active=True)
     return render(request, 'Competition/active_competitions.html', {'competitions': list_competitions})
-
-
