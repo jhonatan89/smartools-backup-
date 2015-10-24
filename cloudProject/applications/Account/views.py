767acde319c1a36eacb86409a21340e0fbd33674
Jhonatan Alarcon <jhonatan.alarcon.ariza@gmail.com>
2015-10-24 14:17:41 -0500

    Test opcion 2 for cache

--- cloudProject/applications/Account/views.py
+++ cloudProject/applications/Account/views.py
@@ -2,10 +2,10 @@ import json
 
 from django.shortcuts import  HttpResponseRedirect,HttpResponse,render_to_response
 from django.contrib import auth
-from session import Session
+#from session import Session
 from django.contrib.auth.models import User, Group
 from django.views.generic import TemplateView
-from cloudProject.applications.MongoDB_APP.Company import Company
+#from cloudProject.applications.MongoDB_APP.Company import Company
 
 
 def signout(request):
@@ -22,19 +22,29 @@ def forbidden(request):
 class signin(TemplateView):
 
     def post(self,request,*args, **kwargs):
-
-        company = Company()
-
+        diccionario={}
         username = request.POST.get('username')
         password = request.POST.get('password')
+        user = auth.authenticate(username=username, password=password)
 
-        if company.validate_signin(username,password):
-            print "antes" + username
-            Session().do_login(username)
-            print "guardo"
+        if user is not None and user.is_active:
+            # Correct password, and the user is marked "active"
+            auth.login(request, user)
             return HttpResponse(status=200)
         else:
             return HttpResponse(status=401)
+        # company = Company()
+        #
+        # username = request.POST.get('username')
+        # password = request.POST.get('password')
+        #
+        # if company.validate_signin(username,password):
+        #     print "antes" + username
+        #     Session().do_login(username)
+        #     print "guardo"
+        #     return HttpResponse(status=200)
+        # else:
+        #     return HttpResponse(status=401)
 
 class signup(TemplateView):
 
@@ -42,17 +52,47 @@ class signup(TemplateView):
 
         diccionario={}
 
-        company = Company()
-
         username = request.POST.get('username')
 
-        if company.username_exist(username):
+        if User.objects.filter(username=username).exists():
+
             diccionario['msg'] = 'USEREXIST'
+
             return HttpResponse(json.dumps(diccionario),content_type='application/json')
         else:
+
             password = request.POST.get('password')
             email = request.POST.get('email')
-            companyname = request.POST.get('companyname')
-            company.create(username=username,companyName = companyname, email=email, password=password)
+            first_name = request.POST.get('companyname')
+
+            user = User.objects.create_user(username=username, email=email, password=password,first_name=first_name)
+
+            group = Group.objects.get(name='Company')
+
+            user.groups.add(group)
+            user.is_staff=True
+            user.save()
+            user = auth.authenticate(username=username, password=password)
+
+            auth.login(request, user)
             diccionario['msg'] = 'OK'
-        return HttpResponse(json.dumps(diccionario),content_type='application/json')
+            return HttpResponse(json.dumps(diccionario),content_type='application/json')
+
+
+
+        # diccionario={}
+        #
+        # company = Company()
+        #
+        # username = request.POST.get('username')
+        #
+        # if company.username_exist(username):
+        #     diccionario['msg'] = 'USEREXIST'
+        #     return HttpResponse(json.dumps(diccionario),content_type='application/json')
+        # else:
+        #     password = request.POST.get('password')
+        #     email = request.POST.get('email')
+        #     companyname = request.POST.get('companyname')
+        #     company.create(username=username,companyName = companyname, email=email, password=password)
+        #     diccionario['msg'] = 'OK'
+        # return HttpResponse(json.dumps(diccionario),content_type='application/json')
