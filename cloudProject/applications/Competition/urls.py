831291126df07cddb0e5e02eda9ea6eee0b8e4af
Jhonatan Alarcon <jhonatan.alarcon.ariza@gmail.com>
2015-10-25 18:07:52 -0500

    video get url

--- cloudProject/applications/Competition/urls.py
+++ cloudProject/applications/Competition/urls.py
@@ -8,5 +8,5 @@ urlpatterns = patterns('cloudProject.applications.Competition.views',
                        url(r'^active_competitions/$', 'show_all_competitions', name='show_all_competitions'),
                        url(r'^(?P<id_competition>\w+)/finish/$', finish.as_view(), name='finish'),
                        url(r'^(?P<id_competition>\w+)/edit/$', edit.as_view(), name='edit'),
-                       url(r'^(?P<id_competition>\d+)/', include('cloudProject.applications.Video.urls')),
+                       url(r'^(?P<id_competition>\w+)/', include('cloudProject.applications.Video.urls')),
 )
