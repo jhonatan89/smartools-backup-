8550b2161f5156bd1e75878038ab29c3bebf2418
camilo <camiloco@gmail.com>
2015-10-25 12:37:47 -0500

    Test 13 edit competitions

--- cloudProject/applications/Competition/urls.py
+++ cloudProject/applications/Competition/urls.py
@@ -6,7 +6,7 @@ from .views import finish, edit
 urlpatterns = patterns('cloudProject.applications.Competition.views',
                        url(r'^$', 'index', name='init'),
                        url(r'^active_competitions/$', 'show_all_competitions', name='show_all_competitions'),
-                       url(r'^(?P<id_competition>\d+)/finish/$', finish.as_view(), name='finish'),
+                       url(r'^(?P<id_competition>\w+)/finish/$', finish.as_view(), name='finish'),
                        url(r'^(?P<id_competition>\w+)/edit/$', edit.as_view(), name='edit'),
                        url(r'^(?P<id_competition>\d+)/', include('cloudProject.applications.Video.urls')),
 )
