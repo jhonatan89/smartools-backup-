d2e804554ec66ac3cc70833de91c8d7cbefcfab1
camilo <camiloco@gmail.com>
2015-10-26 13:48:26 -0500

    Test 8 upload image

--- cloudProject/applications/Video/views.py
+++ cloudProject/applications/Video/views.py
@@ -3,13 +3,15 @@ from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
 from cloudProject.applications.MongoDB_APP.connection_params import Connection
 from cloudProject.applications.Video.forms import UploadVideo
 from cloudProject.applications.MongoDB_APP.Competition import Competition
+from cloudProject.applications.MongoDB_APP.Video import Video
 from cloudProject.applications.MongoDB_APP.S3Manager import S3Manager
 
 
 # Create your views here.
 
 def upload_video(request, id_competition):
-    competition = Competition.objects.get(id=id_competition)
+    competition = Competition()
+    competition.get(id_competition)
     if request.method == 'POST':
         form = UploadVideo(request.POST, request.FILES)
         if form.is_valid():
@@ -25,19 +27,14 @@ def upload_video(request, id_competition):
             return render(request, 'Video/confirmation.html', {'competition': competition})
     else:
         form = UploadVideo()
-        print competition.name
     return render(request, 'Video/index.html', {'form': form, 'competition': competition})
 
 def confirmation_video(request):
     return render_to_response('Video/confirmation.html')
 
 def get_video(request, id_competition):
-    print "methon:get_video"
-    print "id_competition"
-    print id_competition
     competition = Competition()
     competition.get(id_competition)
-    print competition
     videos_per_page = 50
     url = "/competitions/" + id_competition
     #print url + " url"
