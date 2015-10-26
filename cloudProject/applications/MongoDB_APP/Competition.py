a1b6fa61afb84d7e5ad8bc4768fc3188b3efd7ca
camilo <camiloco@gmail.com>
2015-10-26 15:30:04 -0500

    Test 4 show uloapded Videos

--- cloudProject/applications/MongoDB_APP/Competition.py
+++ cloudProject/applications/MongoDB_APP/Competition.py
@@ -114,5 +114,5 @@ class Competition():
         Connection().db.Competition.update({"_id" : ObjectId(id) }, {"$set": {'active' : 'false'} })
 
     def get_videos(self,id, status):
-        competitions_ids = Connection().db.Company.find_one({ "_id" : ObjectId(id) })['videos']
+        competitions_ids = Connection().db.Competition.find_one({ "_id" : ObjectId(id) })['videos']
         self.videos = Video().get_all_by_ids(competitions_ids, status)
