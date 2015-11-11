4c43265440caec457b3a6aee5b84b41db2fbf860
JHONATAN JAVIER ALARCON ARIZA <jhonatan.alarcon.ariza@gmail.com>
2015-11-11 15:34:41 -0500

    build ffmpeg 2

--- cloudProject/applications/MongoDB_APP/tasks.py
+++ cloudProject/applications/MongoDB_APP/tasks.py
@@ -22,9 +22,9 @@ def video_convert(self,id):
         conn = boto.connect_s3(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
         bucket = conn.get_bucket(AWS_STORAGE_BUCKET_NAME)
         video_file = bucket.get_key(str(video.originalVideoPath))
-        # if not os.path.exists(os.path.dirname(video_file.name)):
-        #     print "entro a crear"
-        #     os.makedirs(os.path.dirname(video_file.name))
+        if not os.path.exists(os.path.dirname(url_tmp + video_file.name)):
+             print "entro a crear"
+             os.makedirs(os.path.dirname(url_tmp + video_file.name))
         print "ejemplo " + url_tmp + str(os.path.basename(video.originalVideoPath))
         video_file.get_contents_to_filename(url_tmp + str(os.path.basename(video.originalVideoPath)))
         video_conv = video.originalVideoPath + '.conv.mp4'
