2ec3c0eccd25afdc8be13461f13f53cd0c9c197f
JHONATAN JAVIER ALARCON ARIZA <jhonatan.alarcon.ariza@gmail.com>
2015-11-11 22:42:07 -0500

    build ffmpeg 4

--- cloudProject/applications/MongoDB_APP/tasks.py
+++ cloudProject/applications/MongoDB_APP/tasks.py
@@ -13,29 +13,30 @@ from cloudProject.celery import app
 def video_convert(self,id):
     print "Entro a la sqs"
     print str(id) + "title"
-    url_tmp = 'media/video/'
+    url_tmp = '/tmp/'
     video = Video()
     video.get(id,"ANY")
+
+
     try:
         conn = boto.connect_s3(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
         bucket = conn.get_bucket(AWS_STORAGE_BUCKET_NAME)
         video_file = bucket.get_key(str(video.originalVideoPath))
-
-        if not os.path.exists(os.path.dirname(url_tmp + video_file.name)):
-             print "entro a crear"
-             os.makedirs(os.path.dirname(url_tmp + video_file.name))
-
-        print "ejemplo " + url_tmp + str(os.path.basename(video.originalVideoPath))
-        video_file.get_contents_to_filename(url_tmp + str(os.path.basename(video.originalVideoPath)))
+        print "video_file " + video_file.name
+        print "if -> " + url_tmp + os.path.dirname(video_file.name)
+        if not os.path.exists(url_tmp + os.path.dirname(video_file.name)):
+            print "entro a crear"
+            os.makedirs(url_tmp + os.path.dirname(video_file.name))
+        video_file.get_contents_to_filename(url_tmp + video_file.name)
         video_conv = video.originalVideoPath + '.conv.mp4'
-        Popen(['ffmpeg', '-i', video_file.name, video_conv]).wait()
+        Popen(['ffmpeg', '-i', url_tmp + video_file.name, url_tmp + video_conv]).wait()
         k = Key(bucket)
         k.key = video_conv
-        k.set_contents_from_filename(video_conv)
+        print "set contents ->" + url_tmp + video_conv
+        k.set_contents_from_filename(url_tmp + video_conv)
         video.convertedVideoPath = video_conv
         video.update_to_uploaded()
-
-        message = '<h2>Hi! ' + video.clientfirtsName + ' ' + video.clientLastName + ',</h2><br>' + '<h3>You already can watch your video in our website</h3>' + '<br>' + '<strong>Video:</strong> ' + video.title + '<br>' + '<strong>Video description:</strong> ' + video.description + '<br>' + 'Thanks' + '<br><br>' + 'Sm@rtTools 2015'
+        message = '<h2>Hola ' + video.clientfirtsName + ' ' + video.clientLastName + ',</h2><br>' + '<h3>You already can watch your video in our website</h3>' + '<br>' + '<strong>Video:</strong> ' + video.title + '<br>' + '<strong>Video description:</strong> ' + video.description + '<br>' + 'Thanks' + '<br><br>' + 'Sm@rtTools 2015'
         send_mail('You already is in the competition ', '', 'smarttoolssaas@gmail.com', [video.clientEmail],fail_silently=False, html_message=message)
     except ValueError:
         print 'There is a error in the convert process'
